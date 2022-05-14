f642d786e597ed4f993083d8f73e08ab9db4a9cd
Cleber Varçal <cleber.varcal@varcalsys.com,br>
2022-05-13 22:49:07 -0300

    incluido dockerfile

--- src/CleanArch.Api/Startup.cs
+++ src/CleanArch.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace CleanArch.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArch.Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
